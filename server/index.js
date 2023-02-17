68d695141eb1bb65abfa8a7b608fd5d991ea0c0f
matias <matiasahrensdorf@gmail.com>
2023-02-16 19:04:24 -0500

    establish server connection

--- server/index.js
+++ server/index.js
@@ -1,11 +1,19 @@
 const express = require ('express');
 const app = express();
+const mysql = require('mysql');
 const PORT = 3001;
 
+const db = mysql.createConnection({
+    user: 'root',
+    host: 'localhost',
+    password: 'Glevy97!',
+    database: 'PasswordManager',
+
+});
 
 
 app.get('/', (req, res) => {
-    res.send("Hello World");
+    res.send("goodday world");
 });
 
 
