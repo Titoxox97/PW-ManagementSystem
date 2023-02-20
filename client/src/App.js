e8e0f3785fb1ccfa7ab86b218351687f429af81f
matias <matiasahrensdorf@gmail.com>
2023-02-20 15:29:17 -0500

    create onChange function which will store the updated info in the input

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,31 @@
 import './App.css';
+import { useState } from 'react';
+
 function App() {
+
+  const [password, setPassword] = useState('')
+  const [title, setTitle] = useState('')
+
+
   return ( 
   <div className="App">
     <div className='AddPw'>
-      <input type="text" placeholder="password123" />
-      <input type="text" placeholder="E-mail" />
+      <input 
+      type="text" 
+      placeholder="password123" 
+//When password is changed the change is recorded in the back end
+//onChange is called each time there is a change to the input
+      onChange={(event) => {
+        setPassword(event.target.value);
+        }}
+      />
+      <input 
+      type="text" 
+      placeholder="E-mail" 
+      onChange={(event) => {
+        setTitle(event.target.value);
+      }}
+      />
       <button> Add Password </button>
     </div>
   </div>
