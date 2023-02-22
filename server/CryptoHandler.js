e54977e634ed12a16a1e5c0e5b5dc5636e2b68cb
matias <matiasahrensdorf@gmail.com>
2023-02-21 19:23:17 -0500

    create cipher

--- server/CryptoHandler.js
+++ server/CryptoHandler.js
@@ -1,8 +1,16 @@
 const crypto = require('crypto');
-const encrypt = (password)=> {
 
-}
+// Secret variable necessary for encryption
+const ghost = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+
+const encrypt = (password)=> {
+    const iv = Buffer.from(crypto.randomBytes(16));
+    //1)algorithm 2)transform secret var into buffer 3)pass iv to create cipher
+    const cypher = crypto.createCipheriv('aes-256-ocb',Buffer.from(ghost), iv)
+};
 
 const decrypt = (encryption)=> {
 
-}
+};
+
+module.exports = { encrypt, decrypt };
