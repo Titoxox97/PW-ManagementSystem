870b24ee4d6cc6f22fcb6242fb240064ca1c81fb
matias <matiasahrensdorf@gmail.com>
2023-02-26 16:44:46 -0500

    convert buffer to hex then wrote function to deciopher and convert back to buffer.

--- server/CryptoHandler.js
+++ server/CryptoHandler.js
@@ -5,8 +5,20 @@ const ghost = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
 
 const encrypt = (password)=> {
     const iv = Buffer.from(crypto.randomBytes(16));
+
     //1)algorithm 2)transform secret var into buffer 3)pass iv to create cipher
     const cypher = crypto.createCipheriv('aes-256-ocb',Buffer.from(ghost), iv)
+
+    //Buffer that concatenates the updated pw and the final pw coming out of the cipher
+    const encryptedPw = Buffer.concat([
+        cypher.update(password),
+        cypher.final(),
+    
+    ]);
+    // Converting encrypted password to a string in herxadecimals
+    return { 
+        iv: iv.toString("hex"), 
+        password: encryptedPw.toString("hex") };
 };
 
 const decrypt = (encryption)=> {
